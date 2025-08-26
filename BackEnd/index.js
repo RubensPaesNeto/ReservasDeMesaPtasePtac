138e587e95a3732f00c4035f65ed52e3904924fe
Gustavo6900 <161074983+Gustavo6900@users.noreply.github.com>
2025-08-26 17:33:02 -0400

    aula26/08

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express()
 
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 
 const UsuariosRoutes = require("./routes/UsuarioRoute.js")
 app.use("/usuarios", UsuariosRoutes)
 
-app.listen(51213, (err) => {
-    console.log("aplicação rodandoem localhost:51213")
+app.listen(3000, (err) => {
+    console.log("aplicação rodandoem localhost:5555")
 });
--- BackEnd/routes/UsuarioRoute.js
+++ BackEnd/routes/UsuarioRoute.js
@@ -4,4 +4,6 @@ const UsuarioController = require("../controllers/UsuarioController");
 
 router.post("/cadastro", UsuarioController.cadastrar);
 
+router.post("/login", UsuarioController.login)
+
 module.exports = router
