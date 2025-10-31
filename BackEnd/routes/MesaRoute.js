8362e038579a88d405b8b448d7cc269cd503e737
RubensPaes <rubenspaesneto@gmail.com>
2025-10-31 14:34:36 -0400

    Update MesaRoute.js
    feita a rota para Admin(Backup)

--- BackEnd/routes/MesaRoute.js
+++ BackEnd/routes/MesaRoute.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const MesaController = require("../controllers/MesaController");
+const UsuarioController = require("../controllers/UsuarioController");
 
-router.post("/cadastro", MesaController.cadastrar);
+router.post("/cadastro", UsuarioController.verificaAutenticacao, UsuarioController.verificaIsAdmin, MesaController.cadastrar);
 module.exports = router
